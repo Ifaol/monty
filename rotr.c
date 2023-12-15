beeed938c7f728cf275ab7997f4fdb3f2b58940f
Ifaol <ifaolhunde@gmail.com>
2023-12-15 07:29:13 -0800

    rotr function updated for betty style compliance

--- rotr.c
+++ rotr.c
@@ -8,10 +8,12 @@
 */
 void f_rotr(stack_t **head, unsigned int counter)
 {
-stack_t* prev = NULL;
-stack_t* current = *head;
-stack_t* next = NULL;
+stack_t *prev = NULL;
+stack_t *current = *head;
+stack_t *next = NULL;
 (void) counter;
+if (current == NULL)
+return;
 while (current != NULL)
 {
 next = current->next;
